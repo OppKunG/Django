3d315d66ef140cea46dc91a47215081d8b925b60
Jakarin Chonchumrus <67791331+OppKunG@users.noreply.github.com>
2023-06-05 15:29:13 +0900

    Initial

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.index, name='index') # Home page
-    
+    path('', views.index, name='index'),
+    path('counter',views.counter,name='counter')
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# python manage.py runserver
+
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Hey, Welcome</h1>')
+   return render(request,'index.html')
+
+def counter(request):
+   text= request.GET['text']
+   return render(request, 'counter.html')
